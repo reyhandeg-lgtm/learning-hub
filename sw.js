4face17814b874f867e47a419dde9fa421cb84c8
reyhandeg-lgtm <reyhan.deg@gmail.com>
2026-08-13 03:25:06 -0400

    Add initial learning app files

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='dispersant-learning-v1';
+const ASSETS=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
