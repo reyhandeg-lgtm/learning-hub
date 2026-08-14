d6be9138fc27004474dd38c583a17a8a66b938b2
reyhandeg-lgtm <reyhan.deg@gmail.com>
2026-08-14 13:56:46 -0400

    Add IDS.333 Module 2 to Learning Hub
    Add Module 2 audio, video, MIT lesson and lecture-deck resources, with refreshed offline caching.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='learning-hub-2026-08-14';
+const CACHE='learning-hub-2026-08-14-module2';
 const ASSETS=['./','index.html','video-player.html','audio-player.html','curriculum.js','report.js','google-drive-config.js','google-drive-sync.js','jspdf.umd.min.js','jspdf-LICENSE.txt','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys()
